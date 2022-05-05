ae8583603dc042ab85a70e3b46089841edf6b823
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-05-05 11:34:43 -0300

    first commit

---
+++ users/src/main/java/com/florencio/user/back/end/UsersApplication.java
@@ -0,0 +1,13 @@
+package com.florencio.user.back.end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApplication.class, args);
+	}
+
+}
